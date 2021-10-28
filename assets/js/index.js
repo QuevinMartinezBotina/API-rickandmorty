e0473c98aa2f69960fd0aa8dbf278ada98e1cb03
quevinmartinezbotina <quevinmartinezbotina@gmail.com>
2021-10-28 14:46:20 -0500

    Api consumida

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,17 @@
-const fetchData = require("./utils/fetchData");
-const API = 'https://rickandmortyapi.com/api/character/';
+const API = "https://rickandmortyapi.com/api/character/"
+const HTMLResponse = document.querySelector('#app');
 
-fetchData(API)
-    .then(data => {
-        console.log(data.info.count);
-        return fetchData(`${API}${data.results[0].id}`)
-    })
-    .then(data => {
-        console.log(data.name);
-        return fetchData(data.origin.url)
-    })
-    .then(data => {
-        console.log(data.dimension);
-    })
-    .catch(error => console.log(error));
+fetch(`${API}`)
+    .then((response) => response.json())
+    .then((rick) => {
+        const tpl = rick.results.map((rikoso) => ` 
+            <figure figure class= 'hover-img col-md-3 col-12 rounded p-2'>
+                <img class="rounded" src="${rikoso.image}" />
+                <p class="h4 mt-4">${rikoso.name}</p>
+                <figcaption>
+                    <h3>${rikoso.name}</h3>
+                </figcaption>
+                <p class="mt-3 h5"> <i class="text-success " >${rikoso.status}</i>  <i class="">${rikoso.origin.name}</i>
+            </figure > `);
+        HTMLResponse.innerHTML = `${tpl}`;
+    });
